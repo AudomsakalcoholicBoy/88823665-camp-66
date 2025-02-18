366b948280c4a1f3b4af92ed4e91f1d91edc9f64
Lincon001 <audomsak.kofak@gmail.com>
2025-02-18 15:13:26 +0700

    Update

--- mylaravel/resources/views/product.blade.php
+++ mylaravel/resources/views/product.blade.php
@@ -1,19 +1,18 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 @extends('layouts.default_with_menu')
 
 @section('content')
-    <form action="{{ url('product') }}" method="post">
+    <form action="{{ url('/product') }}" method="post">
         @csrf
         <div class="row">
             <div class="col-6">
-                <div class="mb-3">
-                    <label for="category" class="form-label">Category Name</label>
-                    <input type="text" name="category" class="form-control" id="category">
-                </div>
+                    <label  class="form-label">Category Name</label>
+                    <input type="text"   name="category_name" class="form-control" >
             </div>
         </div>
-        <button type="button" id="btn-add-product" class="btn btn-primary">
-            + เพิ่ม product
-        </button>
+        <button type="button" id="btn-add-product-list" class="btn btn-primary">+ เพิ่ม product</button>
         <div class="row mt-3" id='product-list'>
             <div class="col-6">
                 <label>Product Name <button type="button"
@@ -21,23 +20,32 @@
                 <input name="product_name[]" type="text" class="form-control" />
             </div>
         </div>
-        <div class="mt-3 row">
-            <button class="btn btn-success" type="submit">บันทึก</button>
-        </div>
-    </form>
-    <table class="mt-3 table">
-        <thead>
-            <tr>
-                <td>#</td>
-                <td>Category Name</td>
-                <td>Product Name</td>
-                <td>User Name</td>
-            </tr>
-        </thead>
-        <tbody>
 
-        </tbody>
-    </table>
+        <div class="mt-3 row">
+            <button type="submit" class="btn btn-success">บันทึก</button>
+        </form>
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Category Name</td>
+                    <td>Product Name</td>
+                    <td>User Name</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1.</td>
+                    <td>Category name</td>
+                    <td>
+                        <ul>
+                            <li>Product 01.</li>
+                            <li>Product 02.</li>
+                        </ul>
+                    </td>
+                    <td>User name</td>
+                </tr>
+        </table>
 @endsection
 
 @section('scripts')
@@ -58,20 +66,20 @@
         $(document).on('click','.btn-delete-product', function(){
             $(this).parent().parent().remove();
         })*/
-       $('#btn-add-product-list').on('click',function(){
-        $('#product-list').append(
-            `<div class="col-6">
-                <label>Product Name <button type="button"
-                                        class="btn btn-danger ml-3 mt-2 mb-2 btn-del-product-list">ลบ</button></label>
-                <input name="product_name[]" type="text" class="form-control" />
-            </div>`)
-       })
+        $('#btn-add-product-list').on('click', function(){
+                $('#product-list').append(
+                    `<div class="col-6">
+                        <label>Product Name <button type="button"
+                                                class="btn btn-danger ml-3 mt-2 mb-2 btn-del-product-list">ลบ</button></label>
+                        <input name="product_name[]" type="text" class="form-control" />
+                    </div>`);
+            });
 
-       $(document).on('click','.btn-delete-product', function(){
-            //console.log('clcik delete')
-            $(this).parent().parent().remove();
-        })
-    });
+            $(document).on('click','.btn-del-product-list', function(){
+                //console.log('clcik delete')
+                $(this).parent().parent().remove();
+            });
+        });
 
 </script>
 @endsection
